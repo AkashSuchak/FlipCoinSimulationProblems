98d9b623bafd928307da0418d4adeb64a4cb5db4
AkashSuchak <suchak.akash@gmail.com>
2021-07-06 00:58:13 +0530

    ADD : Multiple Time Flipping Coin And Showing Results

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,14 +1,33 @@
 #! /bin/bash -x
 
 #AUTHOR : AKASH SUCHAK
-#As a simulator start with Flipping a Coin to Display Heads or Tails as Winner
+#Loop Through Multiple Times of flipping a coin and show number of times head and tail has won
 
-#Random Value 0 or 1
-isCheck=$((RANDOM%2))
+#Taking number from user
+echo "How Many Times You want to Flip the Coin : "
+read -p "Enter Number : " number
 
-#Checking Heads or Tails
-if [ "$isCheck" -eq 0 ]; then
-	echo "Heads is Winner"
+#Variables to take total value
+totalHeads=0
+totalTails=0
+
+#Check the number is numeric value or not
+if [[ $number ]] && [ $number -eq $number 2>/dev/null ] && [ $number -gt 0 ]; then
+	for (( i=0; i<$number; i++ ))
+	do
+		#Random Value 0 or 1
+		isCheck=$((RANDOM%2))
+
+		#Checking Heads or Tails
+		if [ "$isCheck" -eq 0 ]; then
+			totalHeads=$(($totalHeads + 1))
+		else
+			totalTails=$(($totalTails + 1))
+		fi
+	done
+
+	#Display Results
+	echo "Out Of $number : Heads Won $totalHeads times : Tails won $totalTails times"
 else
-	echo "Tails is Winner"
+	echo "Wrong Input !! Enter Number Above 0 Only. E.g  15 "
 fi
